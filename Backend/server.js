a180a026b8a02bcb319d04395b0dc9588c81dc96
unknown <loganwolverine750@gmail.com>
2024-06-09 11:36:59 +0530

    Third commit

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,9 @@ const app=require('./app.js')
 
 
 
-const url='mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.5mzncy3.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0'
+const url='mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.cy9dmdb.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0'
 const databaseUser='chaudharyabhishek9675';
-const databasePassword='Abhi123456';
+const databasePassword='abhi987083';
 const databaseName='Image-generator';
 
 let dbLink=url.replace("$_USERNAME_$",databaseUser);
